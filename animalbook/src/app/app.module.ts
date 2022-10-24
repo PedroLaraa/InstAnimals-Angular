f10249631a9b15777e064ecd6854feebe9871d9b
Pedro Lara <pedroalveslara@gmail.com>
2022-10-24 17:56:11 -0300

    ble

---
+++ animalbook/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { HomeModule } from './components/home/home.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
